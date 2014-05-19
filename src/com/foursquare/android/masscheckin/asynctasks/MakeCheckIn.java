53a97295aeace5461b8a2a7aae347cdfe9d7c298
Ali Yeşilkanat <aliyesilkanat@gmail.com>
2014-05-19 21:14:58 +0300

    Group'taki her Friend için checkin yapma gerçekleştirildi

--- src/com/foursquare/android/masscheckin/asynctasks/MakeCheckIn.java
+++ src/com/foursquare/android/masscheckin/asynctasks/MakeCheckIn.java
@@ -1,6 +1,8 @@
 package com.foursquare.android.masscheckin.asynctasks;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +14,8 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -21,6 +25,8 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.foursquare.android.masscheckin.R;
+import com.foursquare.android.masscheckin.classes.DateSingleton;
+import com.foursquare.android.masscheckin.classes.Friends;
 import com.foursquare.android.masscheckin.classes.Venue;
 
 public class MakeCheckIn extends AsyncTask<Object, View, Activity> {
@@ -64,7 +70,7 @@ public class MakeCheckIn extends AsyncTask<Object, View, Activity> {
 					TextView text = ((TextView) view
 							.findViewById(android.R.id.text2));
 					text.setText("Checking you in...");
-		
+
 				}
 			});
 
@@ -73,15 +79,47 @@ public class MakeCheckIn extends AsyncTask<Object, View, Activity> {
 					.get(position).venueId));
 			nameValuePairs.add(new BasicNameValuePair("oauth_token",
 					Venue.ACCESS_TOKEN));
+			if (Venue.groupCheckInMode) {
+				String shout = "with";
+				String mention = "";
+				int mentionBegin = 5;
+				int mentionEnd;
+				for (Friends friend : Venue.activeGroup.friendList) {
+					shout += " " + friend.firstName + ",";
+					mentionEnd = mentionBegin + friend.firstName.length();
+					mention += mentionBegin + "," + mentionEnd + ","
+							+ friend.id+";";
+
+					mentionBegin = mentionEnd + 2;
+				}
+				mention=mention.substring(0,mention.length()-1);
+				shout = shout.substring(0, shout.length() - 1);
+				nameValuePairs.add(new BasicNameValuePair("shout", shout));
+				nameValuePairs.add(new BasicNameValuePair("mentions",mention));
+				
+			}
 
 			// gecici olarak alttaki satiri tut, program çalistirirken
 			// sil
 			nameValuePairs.add(new BasicNameValuePair("broadcast", "private"));
-
-			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			httppost.setHeader("Content-Type",
+					"application/x-www-form-urlencoded;charset=UTF-8");
+			nameValuePairs.add(new BasicNameValuePair("v", DateSingleton
+					.getDate()));
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
 
 			// Execute HTTP Post Request
 			HttpResponse response = httpclient.execute(httppost);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent(), "UTF-8"));
+			String json = reader.readLine();
+
+			try {
+				JSONObject jsonObject = new JSONObject(json);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			if (loadType == LoadVenues.CONST_LOADVENUES)
 				checkedInVenuesIds[position] = true;
 			else if (loadType == LoadVenues.CONST_SUGGESTVENUES)
@@ -97,8 +135,9 @@ public class MakeCheckIn extends AsyncTask<Object, View, Activity> {
 					ListView listv = null;
 					if (loadType == LoadVenues.CONST_LOADVENUES)
 						listv = (ListView) act.findViewById(R.id.lvVenues);
-					else if(loadType == LoadVenues.CONST_SUGGESTVENUES)
-						listv=(ListView) act.findViewById(R.id.lvSearchVenues);
+					else if (loadType == LoadVenues.CONST_SUGGESTVENUES)
+						listv = (ListView) act
+								.findViewById(R.id.lvSearchVenues);
 					SimpleAdapter adapter = (SimpleAdapter) listv.getAdapter();
 					adapter.notifyDataSetChanged();
 				}
--- src/com/foursquare/android/masscheckin/classes/Friends.java
+++ src/com/foursquare/android/masscheckin/classes/Friends.java
@@ -12,7 +12,6 @@ public class Friends implements Comparable<Friends> {
 	public boolean hasPhoto;
 	public boolean isSelected;
 	public String id;
-	
 	public String getSmallPhoto() {
 		if (hasPhoto)
 			return photoPrefix + "128x128" + photoSuffix;
